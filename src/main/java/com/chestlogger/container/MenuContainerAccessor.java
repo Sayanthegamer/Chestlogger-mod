db084a570105d11536dffa9f0e8cd707b4d06fb5
sayanthegamer <sayanbnk2008@gmail.com>
2026-08-18 10:47:27 +0530

    refactor(cleanup): Remove legacy root src directory and obsolete pre-modularization artifacts

--- src/main/java/com/chestlogger/container/MenuContainerAccessor.java
+++
@@ -1,9 +0,0 @@
-package com.chestlogger.container;
-
-/**
- * Duck-typing interface injected into AbstractContainerMenu to store container coordinates and dimension.
- */
-public interface MenuContainerAccessor {
-    ContainerContext chestlogger$getContainerContext();
-    void chestlogger$setContainerContext(ContainerContext context);
-}
